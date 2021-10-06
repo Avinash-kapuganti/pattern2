427983f4aeefc1bed2a78b7b9c0769be073415b9
Bhargav <bhargavkadali39@gmail.com>
2021-10-06 09:52:27 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,7 +1,2 @@
-n=5
-num=0
-for i in range(1,n+1):
-    for j in range(0,i):
-        num+=1
-        print(num,end=" ")
-    print()    
+n = int(input('Enter number of rows:'))
+[print(str(i)*i) for i in range (n+1)]
